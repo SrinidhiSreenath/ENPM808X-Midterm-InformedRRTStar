4b9ef2f3eda94e8cc5c4213659a5e6b535f20cf8
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-10-10 01:47:38 -0400

    Task 2.1: Add source file for class RRTNode containing the class member functions without implementation

---
+++ app/RRTNode.cpp
@@ -0,0 +1,58 @@
+/****************************************************************************
+ * MIT License
+ * Copyright (c) 2018 Srinidhi Sreenath
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
+/**
+ *  @file    RRTNode.cpp
+ *  @author  Srinidhi Sreenath (SrinidhiSreenath)
+ *  @date    10/10/2018
+ *  @version 1.0
+ *
+ *  @brief RRTNode class definition
+ *
+ *  @section DESCRIPTION
+ *
+ *  Source file for class RRTNode. The class is used to hold information
+ *  regarding a node in the workspace. A node consists of state i.e cartesian
+ *  coordinates [x,y] and its parent node in the tree.
+ *
+ */
+
+#include "RRTNode.hpp"
+
+RRTNode::RRTNode() {}
+
+RRTNode::~RRTNode() {}
+
+void RRTNode::setState(const double &x, const double &y) { return; }
+
+std::vector<double> RRTNode::getState() { return 0; }
+
+void RRTNode::setParent(const std::shared_ptr<RRTNode> &parentObject) {
+  return;
+}
+
+std::shared_ptr<RRTNode> RRTNode::getParent() { return; }
+
+void RRTNode::setCostToCome(const double &cost) { return; }
+
+double RRTNode::getCostToCome() { return 0; }
