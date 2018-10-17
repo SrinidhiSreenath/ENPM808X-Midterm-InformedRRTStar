1cac32d9fa9858fc6b7221ebce51418df1bcb19b
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-10-16 22:26:19 -0400

    Task 4.1: Define the class Informed RRT Star which is derived from class RRT Star

--- include/InformedRRTStar.hpp
+++ include/InformedRRTStar.hpp
@@ -1,11 +1,16 @@
-#include "include/RRTStar.hpp"
+#include "RRTStar.hpp"
+#ifndef INCLUDE_INFORMEDRRTSTAR_HPP_
+#define INCLUDE_INFORMEDRRTSTAR_HPP_
+class InformedRRTStar : public RRTStar {
+ private:
+  double cmin_ = 0.0;
+  double cmax_ = 0.0;
 
-class InformedRRTStar : public RRTStar
-{
-  private:
-    double cmin_;
-    double cmax_;
-
-  public:
-    bool isHeuristicNode(const std::vector<double> &newNode);
+ public:
+  InformedRRTStar();
+  ~InformedRRTStar();
+  std::vector<double> generateHeuristicNode();
+  void runPlanner();
+  void resetPlanner();
 };
+#endif  //  INCLUDE_INFORMEDRRTSTAR_HPP_
