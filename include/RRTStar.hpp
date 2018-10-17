695ab7e03e571a53c8dfd9959a100b540d99d7d4
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-10-16 21:19:13 -0400

    Task 3.1: Define the RRT Star derived class from RRT class

--- include/RRTStar.hpp
+++ include/RRTStar.hpp
@@ -1,11 +1,41 @@
-#include "include/RRT.hpp"
+#include "RRT.hpp"
 
 class RRTStar : public RRT {
  private:
-  const double rewireRange_ = 2.0;
+  std::pair<size_t, double> plannerParams_ = getPlannerParameters();
+  size_t maxIterations_ = plannerParams_.first;
+  std::vector<RRTNode> RRTStarTree = getRRTree();
+  const double rewireRange_ = 2.0 * plannerParams_.second;
   std::vector<std::shared_ptr<RRTNode>> rewireNodes_;
+  std::shared_ptr<RRTNode> goalNodePtr;
 
  public:
-  void rewireRRTree(RRTNode &rewireNode,
-                    const std::shared_ptr<RRTNode> &rewiredParent);
+  RRTStar();
+
+  ~RRTStar();
+
+  std::pair<size_t, double> getPlannerParams();
+
+  void setStartAndGoal(const std::vector<double> &start,
+                       const std::vector<double> &goal);
+
+  std::shared_ptr<RRTNode> findClosestTreeNode(
+      const std::vector<double> &randomNode);
+
+  std::shared_ptr<RRTNode> rewireRRTree(
+      const std::vector<double> &newNode,
+      std::shared_ptr<RRTNode> &currentParent);
+
+  void appendRRTree(const std::vector<double> &validNode,
+                    const std::shared_ptr<RRTNode> &validNodeParent);
+
+  std::vector<std::pair<double, double>> getPlannerPath();
+
+  std::shared_ptr<RRTNode> getGoalNodePtr();
+
+  std::vector<RRTNode> getRRTStarTree();
+
+  void runPlanner();
+
+  void resetPlanner();
 };
